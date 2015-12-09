f23896d55725ac1acd0244cefb204f2ccda49775
long <1349052511@qq.com>
2015-12-09 17:58:00 +0800

    regular cimmit

--- init.el
+++ init.el
@@ -3,8 +3,14 @@
  '(menu-bar-mode nil)
  '(org-agenda-files nil)
  '(scroll-bar-mode nil)
- '(tool-bar-mode nil))
- ;;隐藏工具条
+ '(tool-bar-mode nil)
+ '(blink-cursor-mode nil)
+ '(global-linum-mode 1)
+ '(show-paren-mode 1)
+ ) 
+
+;;行号的限制
+(setq line-number-display-limit 1000)
 
 ;;颜色配置
 (set-background-color "black");;背景色
@@ -16,7 +22,20 @@
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
 (add-to-list 'package-archives '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/"))
-
 (custom-set-faces
+)
 
- )
+;;设置org-capture全局快捷键
+(define-key global-map "\C-cc" 'org-capture)
+;;设置capture模板
+(setq org-capture-templates
+      '(("j" "Journal" entry (file+datetree "E:/lisp/capture/emacs/journal.org")
+	 "* %?\nEntered on %U\n  %i\n  %a")
+	("t" "Todo" entry (file+headline "E:/lisp/capture/emacs/gtd.org" "Tasks")
+	 "* TODO %?\n  %i\n  %a")))
+;;快速启动日记
+(define-key global-map "\C-cj"
+  (lambda () (interactive) (org-capture nil "j")))
+;;快速启动todo
+(define-key global-map "\C-ct"
+  (lambda () (interactive) (org-capture nil "t")))
